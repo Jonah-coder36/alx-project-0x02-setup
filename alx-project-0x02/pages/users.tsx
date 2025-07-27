009751ae639730b76f42e721fd203052c6dfe51f
Yona <yonaakuma@gmail.com>
2025-07-27 14:02:23 +0300

    Update users.tsx

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -1,42 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Header from '@/components/layout/Header';
 import UserCard from '@/components/common/UserCard';
-import { UserProps } from '@/interfaces';
+import { type UserProps } from '@/interfaces';
 
-const UsersPage: React.FC = () => {
-  const [users, setUsers] = useState<UserProps[]>([]);
-  const [loading, setLoading] = useState(true);
+interface UsersPageProps {
+  users: UserProps[];
+}
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const res = await fetch('https://jsonplaceholder.typicode.com/users');
-        const data = await res.json();
-        setUsers(data);
-      } catch (err) {
-        console.error('Error fetching users:', err);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchUsers();
-  }, []);
-
+const UsersPage: React.FC<UsersPageProps> = ({ users }) => {
   return (
     <div>
       <Header />
       <div className="p-6">
         <h1 className="text-3xl font-bold mb-6">Users</h1>
-
-        {loading ? (
-          <p>Loading users...</p>
-        ) : (
-          users.map((user) => <UserCard key={user.id} {...user} />)
-        )}
+        {users.map((user) => (
+          <UserCard key={user.id} {...user} />
+        ))}
       </div>
     </div>
   );
 };
 
 export default UsersPage;
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+    props: {
+      users: data,
+    },
+  };
+}
